de78f4a41549daec3437db7acdbf04e8ad88dc13
Anhnhi Tran <anhtran811@gmail.com>
2022-09-07 20:15:42 -0600

    Added places

--- friends.rb
+++ friends.rb
@@ -3,3 +3,6 @@ arrays = ["Joe", "Sarah", "Sam", "Mark", "Jessica"]
 puts arrays[2]
 puts arrays[4]
 puts arrays[0]
+
+places = ["Italy", "France", "Australia", "Japan", "Antartica"]
+puts places.pop
